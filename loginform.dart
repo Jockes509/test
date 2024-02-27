ce4b647b6aa5792ef03a4b07a6159c61802f7d70
Steeve_Emmanuel <148838591+Jockes509@users.noreply.github.com>
2024-02-27 13:31:25 -0500

    Update loginform.dart

--- loginform.dart
+++ loginform.dart
@@ -4,10 +4,35 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Login Form'),
+        title: Text('TP-Login Form'),
       ),
       body: Center(
-       
+       child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            Row(
+              children: [
+                Checkbox(value: false, onChanged: (bool? value) {}),
+                Text('Remember me'),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Submit'),
+            ),
+          ],
+        ),
       ),
     ),
   ));
